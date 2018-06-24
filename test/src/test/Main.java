1387574b6933fa6b1275356f645266d38980d923
mariri <fily.maruri@gmail.com>
2018-06-24 12:28:35 +0900

    brac

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -3,7 +3,8 @@ package test;
 public class Main {
 
 	public static void main(String[] args) {
-		int i = 0;
-		System.out.println(i);
+		for(int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
 	}
 }
